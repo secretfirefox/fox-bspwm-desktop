c5026f1d90635625e55ff8f3009c709712e6714f
secretfirefox <141532262+secretfirefox@users.noreply.github.com>
2025-09-30 23:02:38 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
+# This script was written by Secret Firefox.
+# This will install my custom bspwm setup on your Arch Linux system.
+# Enjoy! 
+
 # Update the system
 
 sudo pacman -Syu
 
-# Install packages
+# Install core packages
 
-sudo pacman -S xorg xorg-xsetroot xorg-xinit bspwm sxhkd gvfs kwallet kitty --noconfirm --needed
+sudo pacman -S xorg xorg-xsetroot xorg-xinit xorg-xrandr bspwm sxhkd kitty polybar feh ttf-fira-sans ttf-fira-mono rofi nano xdg-user-dirs xdg-user-dirs-gtk xdg-desktop-portal xdg-desktop-portal-gtk maim xclip polkit lxsession ffmpegthumbnailer noto-fonts-cjk noto-fonts-emoji --noconfirm --needed
+
+# Install convenient packages
+
+sudo pacman -S pcmanfm galculator lxappearance leafpad gpicview xarchiver --noconfirm --needed
+
+# Install extra packages
+
+#sudo pacman -S fastfetch htop ibus ibus-anthy numlockx git go base-devel 7zip virt-manager qemu-full kwallet flatpak gimp --noconfirm --needed
 
 # Create directories
 
